e488fd684312ead3f76e58cdadd8ced4708ba2aa
Harshit Mishra <165307122+1harshit234@users.noreply.github.com>
2024-05-31 00:08:08 -0700

    Add files via upload

---
+++ question_zerodivisonerror.py
@@ -0,0 +1,16 @@
+x = int(input("write which you want"))  
+try:
+     y = 2/x
+     print(y)
+except IndentationError:
+      
+      
+    print("inavalid valure")
+    
+
+
+print("write some value of x")
+
+
+     
+
